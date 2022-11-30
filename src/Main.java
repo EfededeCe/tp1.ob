0879da6b934dd453b8048ee2be7b9bfe8993986a
Fede dC <fede_delcorro@hotmail.com>
2022-11-30 19:01:55 -0300

    primera entrega

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // System.out.println("Hello world!");
+        int resultado = suma(2,5,10);
+        System.out.println(resultado);
+
+        Coche miCoche = new Coche();
+        miCoche.incrementaPuertas();
+        System.out.println(miCoche.puertas);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
+
+
+class Coche{
+    int puertas = 3;
+    public void incrementaPuertas(){
+        this.puertas++;
     }
 }
+
